c1a2de13e2bd131f97e0e664de79c95020ab204e
Jay G <playerjayho@gmail.com>
2017-12-08 16:38:25 +0000

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ function getFirstSelector(selector) {
       return document.querySelector(selector);
 }
 
-function nestedTarget() {
 
+
+function nestedTarget() {
+  return document.querySelector('#nested div div div .target');
 }
 
 function increaseRankBy(n) {
