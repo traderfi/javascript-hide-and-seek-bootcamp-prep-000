c5d66bc71cb4c9c5ff8a3c04bb8337b5bcc39b1f
Jay G <playerjayho@gmail.com>
2017-12-08 17:00:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ function nestedTarget() {
 
 
 
-function increaseRankBy(n) {
-
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for ( let i = 0; i<lis.length; i++){
+    lis[i].innerHTML = parseInt((i+n).toString())
+  }
 }
 
 
