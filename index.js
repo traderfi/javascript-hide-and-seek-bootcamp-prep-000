b3ab99122a0b6976a5133c83d6f9e5e9664d4ba2
Jay G <playerjayho@gmail.com>
2017-12-08 16:38:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ function getFirstSelector(selector) {
       return document.querySelector(selector);
 }
 
-
-
 function nestedTarget() {
   return document.querySelector('#nested div div div .target');
 }
 
+
+
 function increaseRankBy(n) {
 
 }
+
+
 
 function deepestChild() {
 
